31822e5d2ca89d805a092c2ca3a45eba0ca30152
Kyungcheol <kcfigaro@gmail.com>
2019-03-02 11:23:21 +0000

    chapter03 backend configuration

--- code/terraform/03-terraform-state/file-layout-example/global/s3/main.tf
+++ code/terraform/03-terraform-state/file-layout-example/global/s3/main.tf
@@ -1,7 +1,3 @@
-terraform {
-  required_version = ">= 0.8, < 0.9"
-}
-
 provider "aws" {
   region = "us-east-1"
 }
@@ -17,3 +13,19 @@ resource "aws_s3_bucket" "terraform_state" {
     prevent_destroy = true
   }
 }
+
+## =============================================================================
+##  Note: S3 버킷이 생성된 후에 아래 코드 주석을 해제 하고 terraform init을 수행
+##        백엔드는 채움 참조 구문을 지원하지 않기 때문에 직접 입력
+##        terraform.backend: configuration cannot contain interpolations
+##        참조 - https://www.terraform.io/docs/backends/config.html
+## =============================================================================
+
+#terraform {
+#  backend "s3" {
+#    bucket = "(생성한 버킷 이름)"
+#    key    = "terraform.tfstate"
+#    region = "us-east-1"
+#    encrypt = true
+#  }
+#}
