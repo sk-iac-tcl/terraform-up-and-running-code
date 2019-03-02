1003341a453c96a80ee7efa8053a3268379ad368
Kyungcheol <kcfigaro@gmail.com>
2019-03-02 16:26:39 +0000

    update version

--- code/terraform/05-tips-and-tricks/loops-and-if-statements/live/global/existing-iam-user/main.tf
+++ code/terraform/05-tips-and-tricks/loops-and-if-statements/live/global/existing-iam-user/main.tf
@@ -1,13 +1,9 @@
-terraform {
-  required_version = ">= 0.8, < 0.9"
-}
-
 provider "aws" {
   region = "us-east-1"
 }
 
 resource "aws_iam_user" "existing_user" {
-  # You should change this to the username of an IAM user that already
-  # exists so you can practice using the terraform import command
+  # 실제로 계정에 이미 존재하는 IAM 사용자 이름으로 아래를 변경해야 하며
+  # terraform import 명령어로 기존 리소스를 테라폼에 추가할 수 있다.
   name = "yevgeniy.brikman"
 }
